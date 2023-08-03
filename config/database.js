1a2ca9c14b419a810038d8a77c2923430b7a34d9
Tentraby <98299414+Tentraby@users.noreply.github.com>
2023-08-03 14:53:15 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('railway', 'root', 'ME7xIRWDUCpTGdzkmnN2', {
-  host: 'containers-us-west-121.railway.app',
-  dialect: 'mysql',
-});
-
+const sequelize = new Sequelize(
+  configDB.DB,
+  configDB.username,
+  configDB.password,
+  {
+    host: configDB.host,
+    port: configDB.port,
+    dialect: 'mysql',
+  }
+);
 module.exports = sequelize;
